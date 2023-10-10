b5992f348455fde36f614c909572752ecf8e22d3
Juho Siitonen <104775829+JuhoSiitonen@users.noreply.github.com>
2023-10-10 23:11:20 +0000

    Added exercise 9.3

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,3 +1,24 @@
+interface parsedBMIArgs {
+  height: number;
+  weight: number;
+}
+
+const parsedArguments = (args: string[]): parsedBMIArgs => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (isNaN(Number(args[2]))) {
+    throw new Error('Provided values were not numbers!');
+  }
+  const values: number = Number(args[2]);
+  if (isNaN(Number(args[3]))) {
+    throw new Error('Provided values were not numbers!');
+  }
+  const targ: number = Number(args[3]);
+  return {
+    height: values,
+    weight: targ
+  }
+}
+
 const bmiCalculator = (height: number, weight: number) : string => {
     const metric = height * 0.01;
     const result = weight / ( metric*metric)
@@ -12,5 +33,15 @@ const bmiCalculator = (height: number, weight: number) : string => {
     }
     
   }
-  
-  console.log(bmiCalculator(180, 74))
+
+  try {
+    const { height, weight } = parsedArguments(process.argv);
+    const result = bmiCalculator(height, weight);
+    console.log(result)
+  } catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+  }
